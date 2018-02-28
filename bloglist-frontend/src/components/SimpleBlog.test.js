4f03cc99717fbf5c48c906e783bb0d04d17b99b9
tviivi <viivi.tiihonen@helsinki.fi>
2018-02-28 14:23:43 +0200

    tehtävä 5.13

--- bloglist-frontend/src/components/SimpleBlog.test.js
+++ bloglist-frontend/src/components/SimpleBlog.test.js
@@ -9,17 +9,37 @@ describe.only('<SimpleBlog />', () => {
             author: 'Testaaja',
             likes: 23
         }
-        console.log(blog)
 
         const simpleBlogComponent = shallow(<SimpleBlog blog={blog} />)
-        console.log(simpleBlogComponent.debug())
 
         const contentDiv = simpleBlogComponent.find('.content')
         const likesDiv = simpleBlogComponent.find('.likes')
 
         expect(contentDiv.text()).toContain(blog.title, blog.author)
-        console.log(contentDiv.debug())
         expect(likesDiv.text()).toContain(blog.likes)
 
     })
+
+    it('clicking the button twice calls event handler twice', () => {
+        const blog = {
+            title: 'Jee testi jee',
+            author: 'Testaaja2',
+            likes: 21
+        }
+      
+        const mockHandler = jest.fn()
+      
+        const blogComponent = shallow(
+          <SimpleBlog
+            blog={blog}
+            onClick={mockHandler}
+          />
+        )
+      
+        const button = blogComponent.find('button')
+        button.simulate('click')
+        button.simulate('click')
+      
+        expect(mockHandler.mock.calls.length).toBe(2)
+      })
 })
